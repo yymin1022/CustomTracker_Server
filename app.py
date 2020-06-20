ecbf1955585e322b80949d6a00cee75ba72e91a5
yymin1022 <yymin1022@gmail.com>
2020-06-20 23:45:47 +0900

    Add Unipass URL

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+apiKey = "d240g270y083v227e030m080h1"
+unipassUrl = str.format("https://unipass.customs.go.kr:38010/ext/rest/cargCsclPrgsInfoQry/retrieveCargCsclPrgsInfo?crkyCn={0}&blYy=2020&hblNo=6077876695710", apiKey)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return unipassUrl
 
 
 if __name__ == '__main__':
